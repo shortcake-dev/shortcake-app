9aebcb293e67e8abcec5e26f375a2ec4b5da27d6
ignormies <bryce.beagle@gmail.com>
2021-03-15 22:21:52 -0700

    Remove comment copied from example

--- test/main_test.dart
+++ test/main_test.dart
@@ -3,7 +3,6 @@ import 'package:shortcake_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
     await tester.pumpWidget(ShortcakeApp());
 
     expect(find.text('Shortcake'), findsOneWidget);
