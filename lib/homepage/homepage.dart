f1b74d242dca4b116185faae9bebfa3d28496ab0
ignormies <bryce.beagle@gmail.com>
2021-04-02 07:05:57 -0700

    Demonstrate making a GraphQL request and display the result in the client

--- lib/homepage/homepage.dart
+++ lib/homepage/homepage.dart
@@ -1,8 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
+import 'package:shortcake/shortcake.dart';
 
-class ShortcakeHomepage extends StatelessWidget {
+class ShortcakeHomepage extends StatefulWidget {
+  @override
+  _ShortcakeHomepageState createState() => _ShortcakeHomepageState();
+}
+
+class _ShortcakeHomepageState extends State<ShortcakeHomepage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text('Shortcake'));
+    return Scaffold(
+      body: FutureBuilder(
+        future: Provider.of<ShortcakeApi>(context, listen: false).introspect(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              return Text('none');
+            case ConnectionState.active:
+            case ConnectionState.waiting:
+              return Text('active/waiting');
+            case ConnectionState.done:
+              return Text(snapshot.data.toString());
+            default:
+              return Text('default');
+          }
+        },
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shortcake/shortcake.dart';
 
 import 'homepage/homepage.dart';
 
 void main() {
-  runApp(ShortcakeApp());
+  runApp(ShortcakeClient());
 }
 
-class ShortcakeApp extends StatelessWidget {
+class ShortcakeClient extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Shortcake', home: ShortcakeHomepage());
+    return Provider<ShortcakeApi>(
+      create: (_) => ShortcakeApi("http://localhost:8000"),
+      child: MaterialApp(title: 'Shortcake', home: ShortcakeHomepage()),
+    );
   }
 }
