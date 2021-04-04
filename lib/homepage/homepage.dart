673d866cead77c4b044d82640c652ed9a29a5299
ignormies <bryce.beagle@gmail.com>
2021-04-03 18:13:14 -0700

    Make a GraphQL request and display result in the client (#16)

--- lib/homepage/homepage.dart
+++ lib/homepage/homepage.dart
@@ -1,8 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:graphql/client.dart';
+import 'package:provider/provider.dart';
+import 'package:shortcake/shortcake.dart';
 
-class ShortcakeHomepage extends StatelessWidget {
+class ShortcakeHomepage extends StatefulWidget {
+  @override
+  _ShortcakeHomepageState createState() => _ShortcakeHomepageState();
+}
+
+class _ShortcakeHomepageState extends State<ShortcakeHomepage> {
+  late Future<QueryResult> _introspectionFuture;
+
+  @override
+  void initState() {
+    super.initState();
+
+    final shortcakeApi = Provider.of<ShortcakeApi>(context, listen: false);
+    _introspectionFuture = shortcakeApi.introspect();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text('Shortcake'));
+    return Scaffold(
+      body: FutureBuilder(
+        future: _introspectionFuture,
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              return Text('none');
+            case ConnectionState.active:
+            case ConnectionState.waiting:
+              return Text('loading');
+            case ConnectionState.done:
+              return Text(snapshot.data.toString());
+            default:
+              return Text('default');
+          }
+        },
+      ),
+    );
   }
 }
--- test/homepage/homepage_test.dart
+++ test/homepage/homepage_test.dart
@@ -1,11 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:graphql/client.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:provider/provider.dart';
+import 'package:shortcake/shortcake.dart';
 import 'package:shortcake_app/homepage/homepage.dart';
 
-void main() {
-  testWidgets('Homepage has content', (WidgetTester tester) async {
-    await tester.pumpWidget(MaterialApp(home: ShortcakeHomepage()));
+class MockApi extends Mock implements ShortcakeApi {}
 
-    expect(find.text('Shortcake'), findsOneWidget);
+class TestHomepage extends StatelessWidget {
+  final ShortcakeApi shortcakeApi;
+
+  const TestHomepage(this.shortcakeApi);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Provider<ShortcakeApi>.value(
+        value: shortcakeApi,
+        child: ShortcakeHomepage(),
+      ),
+    );
+  }
+}
+
+void main() {
+  group("Homepage widget", () {
+    late MockApi mockApi;
+    late QueryResult result;
+
+    setUpAll(() async {
+      mockApi = MockApi();
+
+      result = QueryResult.optimistic(data: {"some_field": 1234});
+      when(() => mockApi.introspect()).thenAnswer((_) async => result);
+    });
+
+    testWidgets('starts by displaying "loading"', (tester) async {
+      var widget = TestHomepage(mockApi);
+
+      // One pump builds widget with uncompleted Future
+      await tester.pumpWidget(widget);
+
+      expect(find.text('loading'), findsOneWidget);
+    });
+
+    testWidgets('then loads and displays results', (tester) async {
+      var widget = TestHomepage(mockApi);
+
+      // One pump builds widget with uncompleted Future
+      await tester.pumpWidget(widget);
+      await tester.pumpAndSettle();
+
+      expect(find.text(result.toString()), findsOneWidget);
+    });
   });
 }
