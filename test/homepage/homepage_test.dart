e7caaa7ea319ed12fed692f3c61705121dd40b1c
ignormies <bryce.beagle@gmail.com>
2021-04-03 17:47:14 -0700

    Abstract the test widget a bit

--- test/homepage/homepage_test.dart
+++ test/homepage/homepage_test.dart
@@ -8,17 +8,28 @@ import 'package:shortcake_app/homepage/homepage.dart';
 
 class MockApi extends Mock implements ShortcakeApi {}
 
+class TestHomepage extends StatelessWidget {
+  final ShortcakeApi shortcakeApi;
+
+  const TestHomepage(this.shortcakeApi);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Provider<ShortcakeApi>.value(
+        value: shortcakeApi,
+        child: ShortcakeHomepage(),
+      ),
+    );
+  }
+}
+
 void main() {
   group("Homepage widget", () {
     final mockApi = MockApi();
 
     testWidgets('starts by displaying "loading"', (tester) async {
-      var widget = MaterialApp(
-        home: Provider<ShortcakeApi>.value(
-          value: mockApi,
-          child: ShortcakeHomepage(),
-        ),
-      );
+      var widget = TestHomepage(mockApi);
 
       var result = QueryResult.optimistic(data: {"some_field": 1234});
       when(() => mockApi.introspect()).thenAnswer((_) async => result);
@@ -30,12 +41,7 @@ void main() {
     });
 
     testWidgets('loads and displays results', (tester) async {
-      var widget = MaterialApp(
-        home: Provider<ShortcakeApi>.value(
-          value: mockApi,
-          child: ShortcakeHomepage(),
-        ),
-      );
+      var widget = TestHomepage(mockApi);
 
       var result = QueryResult.optimistic(data: {"some_field": 1234});
       when(() => mockApi.introspect()).thenAnswer((_) async => result);
